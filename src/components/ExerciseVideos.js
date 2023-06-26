31b5e41a139f60152017992c09499ea689e12195
Simon Gorozabel <sgorozabel@gmail.com>
2023-06-26 06:18:40 -0500

    minor changes

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
 const ExerciseVideos = ({ exerciseVideos, name }) => {
-
-  if(!exerciseVideos.length) return 'Loading...';
+  if (!exerciseVideos.length) return "Loading...";
   return (
     <Box sx={{ marginTop: { lg: "200px", xs: "20px" } }} p="50px">
       <Typography variant="h4" mb="33px">
@@ -14,42 +13,40 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
         exercise videos.
       </Typography>
       <Stack
-      justifyContent='flex-start'
-      flexWrap='wrap'
-      alignItems='center'
-      sx={{
-        flexDirection:{lg:'row'},
-        gap:{lg:'110px',xs:'0'}
-      }}
+        justifyContent="flex-start"
+        flexWrap="wrap"
+        alignItems="center"
+        sx={{
+          flexDirection: { lg: "row" },
+          gap: { lg: "110px", xs: "0" },
+        }}
       >
-          {exerciseVideos?.slice(0, 6).map((item,index)=>(
-            <a
+        {exerciseVideos?.slice(0, 6).map((item, index) => (
+          <a
             key={index}
             className="exercise-video"
             href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
             target="_blank"
             rel="noreferrer"
-            >
-              <img
+          >
+            <img
               src={item.video.thumbnails[0].url}
               alt={item.video.title}
-              />
-              <Box>
-                <Typography 
-                variant="h5"
-                color='#000'
-                >
-                  {item.video.title}
-                </Typography>
-                <Typography 
-                variant="h7"
-                color='#000'
-                >
-                  {item.video.channelName}
-                </Typography>
-              </Box>
-            </a>
-          ))}
+              style={{
+                borderRadius: "10px",
+                margin: "5px",
+              }}
+            />
+            <Box>
+              <Typography variant="h5" color="#000">
+                {item.video.title}
+              </Typography>
+              <Typography variant="h7" color="#000">
+                {item.video.channelName}
+              </Typography>
+            </Box>
+          </a>
+        ))}
       </Stack>
     </Box>
   );
